0cbe1ada69a181c74f5a00966d85bb234742c49c
Nasanbat G <nasanbatg@Nasanbats-MacBook-Pro.local>
2023-04-08 13:17:51 +0800

    [Folder] folder-structure

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import {SafeAreaView, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
