b1636885934f1e56842418efc63aef0bbc844c46
Nasanbat G <nasanbatg@Nasanbats-MacBook-Pro.local>
2023-05-04 18:26:22 +0800

    conf

--- School_Vibes/src/index.tsx
+++ School_Vibes/src/index.tsx
@@ -1,18 +1,22 @@
 import {SafeAreaView} from 'react-native';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Navigator } from './navigations';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Navigator} from './navigations';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Nav" component={Navigator} options={{headerShown: false}}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Nav"
+          component={Navigator}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
