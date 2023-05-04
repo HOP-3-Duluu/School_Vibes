34341488826b7bec5e207388a713ce9104d788cd
EnkhjinHop-3 <enkhjin@iMac-17s-iMac.local>
2023-05-04 19:03:06 +0800

    [navigation] add bottom navigation

---
+++ School_Vibes/src/assets/icon/Calendar.tsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+export const CalendarIcon = ({color}: any) => {
+  return (
+    <Svg
+      width={30}
+      height={30}
+      viewBox="0 0 50 50"
+      fill="none"
+    >
+      <Path
+        d="M39.583 8.333H10.417A4.167 4.167 0 006.25 12.5v29.167a4.167 4.167 0 004.167 4.166h29.166a4.167 4.167 0 004.167-4.166V12.5a4.167 4.167 0 00-4.167-4.167zM33.334 4.167V12.5M33.334 4.167V12.5M16.666 4.167V12.5M6.25 20.833h37.5"
+        stroke={color}
+        strokeWidth={5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  )
+}
--- School_Vibes/src/assets/icon/index.ts
+++ School_Vibes/src/assets/icon/index.ts
@@ -2,3 +2,7 @@ export * from './BulletLeft';
 export * from './BulletRight';
 export * from './Plus';
 export * from './Clock';
+export * from './Home';
+export * from './Profile';
+export * from './Calendar';
+export * from './Task';
