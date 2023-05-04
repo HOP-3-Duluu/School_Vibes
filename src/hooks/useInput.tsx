581f880b2cfcc5ee2a102bc3b091d1e9976480ee
Nasanbat G <nasanbatg@Nasanbats-MacBook-Pro.local>
2023-05-04 18:01:30 +0800

    [add] backend

--- src/hooks/useInput.tsx
+++
@@ -1,9 +0,0 @@
-import {useState} from 'react';
-
-export const useInput = (initialValue: string) => {
-  const [value, setValue] = useState<string>(initialValue);
-  const onChangeText = (text: string) => {
-    setValue(text);
-  };
-  return [value, onChangeText];
-};
--- src/utils/formerDate.tsx
+++
@@ -1,4 +0,0 @@
-export const formatDate = (date: Date): string => {
-  const options: any = {year: 'numeric', month: 'long', day: 'numeric'};
-  return new Intl.DateTimeFormat('en-US', options).format(new Date(date));
-};
