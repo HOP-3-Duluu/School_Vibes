a3446759679766e7ce9293a705be8bc749a1646b
Nasanbat G <nasanbatg@Nasanbats-MacBook-Pro.local>
2023-05-13 12:34:47 +0800

    [API] CRUD

--- School_Vibes/src/providers/AuthProvider.tsx
+++ School_Vibes/src/providers/AuthProvider.tsx
@@ -1,9 +1,18 @@
 import React, {createContext, useContext, useState, ReactNode, FC} from 'react';
 import type {Dispatch, SetStateAction} from 'react';
+import {instance} from '../library';
 
+interface userProps {
+  name: string;
+  password: string;
+}
 interface AuthContextInterface {
-  user?: string;
-  setUser: Dispatch<SetStateAction<string>>;
+  user?: userProps;
+  setUser: Dispatch<SetStateAction<userProps>>;
+  Login: (name: string, password: string, rePassword: string) => void;
+  signUp: (props: any) => void;
+  updateUser: (username: string) => void;
+  deleteUser: (username: string, password: string) => void;
 }
 
 const AuthContext = createContext<AuthContextInterface>(
@@ -12,11 +21,48 @@ const AuthContext = createContext<AuthContextInterface>(
 interface AuthProviderProps {
   children: ReactNode;
 }
-export const AuthProvider: FC<AuthProviderProps> = ({children}) => {
-  const [user, setUser] = useState('');
 
+export const AuthProvider: FC<AuthProviderProps> = ({children}) => {
+  const [user, setUser] = useState<userProps>();
+
+  const Login = (name: string, password: string, rePassword: string) => {
+    try {
+      // const login = await in
+      setUser({name, password});
+    } catch (e) {
+      return e;
+    }
+  };
+
+  const signUp = async (props: any) => {
+    try {
+      await instance.post(`/user`, props);
+      return props;
+    } catch (e) {
+      return e;
+    }
+  };
+
+  const updateUser = async (username: string) => {
+    try {
+      await instance.put(`/user`, {data: {username}});
+      return username;
+    } catch (e) {
+      return e;
+    }
+  };
+
+  const deleteUser = async (username: string, password: string) => {
+    try {
+      await instance.delete(`/user/${username}`, {data: {password}});
+      return username;
+    } catch (error) {
+      throw new Error('Failed to delete user');
+    }
+  };
   return (
-    <AuthContext.Provider value={{user, setUser}}>
+    <AuthContext.Provider
+      value={{user, setUser, Login, signUp, updateUser, deleteUser}}>
       {children}
     </AuthContext.Provider>
   );
--- School_Vibes/src/providers/DataProvider.tsx
+++ School_Vibes/src/providers/DataProvider.tsx
@@ -1,22 +1,92 @@
 import React, {createContext, useContext, useState, ReactNode, FC} from 'react';
 import type {Dispatch, SetStateAction} from 'react';
+import {instance} from '../library';
 
 interface DataContextInterface {
   data?: string;
   setData: Dispatch<SetStateAction<string>>;
+  getData: ({id, type}: {id: string; type: string}) => void;
+  deleteData: ({id, type}: {id: string; type: string}) => void;
+  createGroup: (props: any) => void;
+  createClass: (props: any) => void;
+  updateGroup: (props: any) => void;
+  updateClass: (props: any) => void;
 }
 
 const DataContext = createContext<DataContextInterface>(
   {} as DataContextInterface,
 );
+
 interface DataProviderProps {
   children: ReactNode;
 }
+
 export const DataProvider: FC<DataProviderProps> = ({children}) => {
   const [data, setData] = useState('');
 
+  const getData = async ({id, type}: {id: string; type: string}) => {
+    try {
+      const data = await instance.get(`/${type}/${id}`);
+      return data;
+    } catch (e) {
+      return e;
+    }
+  };
+  const deleteData = async ({id, type}: {id: string; type: string}) => {
+    try {
+      const data = await instance.delete(`/${type}/${id}`);
+      return data;
+    } catch (e) {
+      return e;
+    }
+  };
+
+  const createGroup = async (props: any) => {
+    try {
+      await instance.post('/group', {data: props});
+      return props;
+    } catch (e) {
+      return e;
+    }
+  };
+  const createClass = async (props: any) => {
+    try {
+      await instance.post('/class', {data: props});
+      return props;
+    } catch (e) {
+      return e;
+    }
+  };
+
+  const updateGroup = async (props: any) => {
+    try {
+      await instance.put('/group', {data: props});
+      return props;
+    } catch (e) {
+      return e;
+    }
+  };
+  const updateClass = async (props: any) => {
+    try {
+      await instance.put('/class', {data: props});
+      return props;
+    } catch (e) {
+      return e;
+    }
+  };
+
   return (
-    <DataContext.Provider value={{data, setData}}>
+    <DataContext.Provider
+      value={{
+        data,
+        setData,
+        getData,
+        deleteData,
+        createGroup,
+        createClass,
+        updateGroup,
+        updateClass,
+      }}>
       {children}
     </DataContext.Provider>
   );
