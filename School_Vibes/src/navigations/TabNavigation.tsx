e196138111cf80cfcc35a9b8ec6062541368de8e
Nasanbat G <nasanbatg@Nasanbats-MacBook-Pro.local>
2023-05-16 16:23:24 +0800

    [navigation] Animation

--- School_Vibes/src/navigations/TabNavigation.tsx
+++ School_Vibes/src/navigations/TabNavigation.tsx
@@ -1,86 +1,155 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {HomeIcon, ProfileIcon, CalendarIcon, TaskIcon} from '../assets';
 import Colors from '../constants/Colors';
-import {Calendar, Font} from '../components';
+import {Calendar} from '../components';
 import Test from '../Check';
 import {Profile, Task} from '../screens';
-import {View} from 'react-native';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 const Tab = createBottomTabNavigator();
 
-const BgColor = ({children}: any) => (
-  <View
-    style={{
-      backgroundColor: Colors.background,
-      padding: 8,
-      borderRadius: 50,
-    }}>
-    {children}
-  </View>
-);
+const TabArr = [
+  {
+    route: 'Home',
+    label: 'Home',
+    icon: ({focused}: any) => (
+      <HomeIcon color={focused ? Colors.background : Colors.primary} />
+    ),
+    component: Test,
+  },
+  {
+    route: 'Task',
+    label: 'Task',
+    icon: ({focused}: any) => (
+      <TaskIcon color={focused ? Colors.background : Colors.primary} />
+    ),
+    component: Task,
+  },
+  {
+    route: 'Add',
+    label: 'Add',
+    icon: ({focused}: any) => (
+      <CalendarIcon color={focused ? Colors.background : Colors.primary} />
+    ),
+    component: Calendar,
+  },
+  {
+    route: 'Profile',
+    label: 'Profile',
+    icon: ({focused}: any) => (
+      <ProfileIcon color={focused ? Colors.background : Colors.primary} />
+    ),
+    component: Profile,
+  },
+];
 
-export const TabNavigation = () => {
-  const getTabScreenOptions = (label: string, icon: JSX.Element) => ({
-    tabBarLabel: ({focused}: {focused: boolean}) =>
-      focused && (
-        <Font fontSize={25} color={Colors.primary}>
-          •
-        </Font>
-      ),
-    tabBarIcon: () => icon,
-    name: label,
-  });
+const animate1 = {
+  0: {scale: 0.5, translateY: 0},
+  0.92: {translateY: -34},
+  1: {scale: 1.2, translateY: -24},
+};
+const animate2 = {
+  0: {scale: 1.2, translateY: -24},
+  1: {scale: 1, translateY: 7},
+};
 
-  const tabScreens = [
-    {
-      component: Test,
-      options: getTabScreenOptions(
-        'Home',
-        <BgColor>
-          <HomeIcon color={Colors.primary} />
-        </BgColor>,
-      ),
-    },
-    {
-      component: Task,
-      options: getTabScreenOptions(
-        'Task',
-        <BgColor>
-          <TaskIcon color={Colors.primary} />
-        </BgColor>,
-      ),
-    },
-    {
-      component: Calendar,
-      options: getTabScreenOptions(
-        'Calendar',
-        <BgColor>
-          <CalendarIcon color={Colors.primary} />
-        </BgColor>,
-      ),
-    },
-    {
-      component: Profile,
-      options: getTabScreenOptions(
-        'Profile',
-        <BgColor>
-          <ProfileIcon color={Colors.primary} />
-        </BgColor>,
-      ),
-    },
-  ];
+const circle1 = {
+  0: {scale: 0},
+  0.5: {scale: 0.5},
+  1: {scale: 1},
+};
+const circle2 = {0: {scale: 1}, 1: {scale: 0}};
+
+const TabButton = (props: {
+  item: any;
+  onPress?: () => void;
+  accessibilityState?: any;
+}) => {
+  const {item, onPress, accessibilityState} = props;
+  const focused = accessibilityState.selected;
+  const viewRef = useRef<any>(null);
+  const circleRef = useRef<any>(null);
+  const textRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (focused) {
+      viewRef.current.animate(animate1);
+      circleRef.current.animate(circle1);
+      textRef.current.transitionTo({scale: 1});
+    } else {
+      viewRef.current.animate(animate2);
+      circleRef.current.animate(circle2);
+      textRef.current.transitionTo({scale: 0});
+    }
+  }, [focused]);
 
   return (
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={1}
+      style={styles.container}>
+      <Animatable.View ref={viewRef} duration={1000} style={styles.container}>
+        <View style={styles.btn}>
+          <Animatable.View ref={circleRef} style={styles.circle} />
+          {item.icon({focused})}
+        </View>
+        <Animatable.Text ref={textRef} style={styles.text}>
+          {item.label}
+        </Animatable.Text>
+      </Animatable.View>
+    </TouchableOpacity>
+  );
+};
+
+export const TabNavigation = () => {
+  return (
     <Tab.Navigator>
-      {tabScreens.map((screen, index) => (
-        <Tab.Screen
-          key={index}
-          component={screen.component}
-          options={screen.options}
-          name={screen.options.name}
-        />
-      ))}
+      {TabArr.map((item, index) => {
+        return (
+          <Tab.Screen
+            key={index}
+            name={item.route}
+            component={item.component}
+            options={{
+              tabBarShowLabel: false,
+              tabBarButton: props => <TabButton {...props} item={item} />,
+            }}
+          />
+        );
+      })}
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  btn: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 4,
+    borderColor: Colors.background,
+    backgroundColor: Colors.background,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  circle: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.primary,
+    borderRadius: 25,
+  },
+  text: {
+    fontSize: 10,
+    textAlign: 'center',
+    color: Colors.primary,
+  },
+});
