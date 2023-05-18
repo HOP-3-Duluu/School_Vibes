3e2aeb9f57c1da95b1f17b8a9e1e4e7eccd430fd
2022-hop3-Batjargal <2022-hop3-batjargal@iMAC2021-05s-iMac.local>
2023-05-18 17:51:40 +0800

    [screen] Splash

--- School_Vibes/src/index.tsx
+++ School_Vibes/src/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {AuthProvider, DataProvider} from './providers';
 import {RootNavigation} from './navigations';
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <NavigationContainer>
       <AuthProvider>
